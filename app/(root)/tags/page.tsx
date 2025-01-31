5da20f73f1b5acabe45c4f3ae05c51e34700855e
NjilaBoris <njilaboris2@gmail.com>
2025-01-31 17:26:16 +0100

    Implement getTags server action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,6 +1,14 @@
+import { getTags } from "@/lib/actions/tag.action";
 import React from "react";
 
-function Tags() {
+async function Tags() {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "test",
+  });
+
+  const { tags } = data || {};
   return <div>Tags</div>;
 }
 
